39fcf400b685af1d3f7b49d9f77765c9c667004d
DT7Solutions <developer@dt7solutions.com>
2023-01-19 18:32:46 +0530

    deployement first

--- magsmen/magsmenapp/urls.py
+++ magsmen/magsmenapp/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import Home
+from .views import Home,About,Contact,FAQS,Blogs,Service,Casestudy
 urlpatterns = [
     path('', Home , name='home'),
+    path('about/', About , name='about'),
+    path('faqs/', FAQS , name='faqs'),
+    path('contact/', Contact , name='contact'),
+    path('blogs/', Blogs , name='blogs'),
+    path('service/', Service , name='service'),
+    path('casestudy/', Casestudy , name='casestudy'),
 ]
--- magsmen/magsmenapp/views.py
+++ magsmen/magsmenapp/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request, 'uifiles/home.html')
+
+def About(request):
+    return render(request, 'uifiles/about.html')
+
+def FAQS(request):
+    return render(request, 'uifiles/faqs.html')
+
+def Contact(request):
+    return render(request, 'uifiles/contact.html')
+
+def Blogs(request):
+    return render(request, 'uifiles/blogs.html')
+
+def Service(request):
+    return render(request, 'uifiles/service1.html')
+
+def Casestudy(request):
+    return render(request, 'uifiles/service1.html')
